7989071e80b4ad3209358b43f1997f889e78ffa5
王康 <11510815@mail.sustech.edu.cn>
2022-01-15 20:27:43 +0800

    HJ100 等差数列

---
+++ src/nowcoder/hj/hj100/Main.java
@@ -0,0 +1,16 @@
+package nowcoder.hj.hj100;
+
+import java.util.Scanner;
+
+// 注意类名必须为 Main, 不要有任何 package xxx 信息
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        // 注意 hasNext 和 hasNextLine 的区别
+        while (in.hasNextInt()) { // 注意 while 处理多个 case
+            int n = in.nextInt();
+            int n_th = 3*n-1;
+            System.out.println((n_th+2)*n/2);
+        }
+    }
+}
