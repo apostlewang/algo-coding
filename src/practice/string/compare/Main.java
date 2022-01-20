8f826544c0ea1508a059ca0dae890f96bdbd17b9
王康 <11510815@mail.sustech.edu.cn>
2022-01-20 11:42:25 +0800

    练习String对象的几种比较方法

---
+++ src/practice/string/compare/Main.java
@@ -0,0 +1,41 @@
+package practice.string.compare;
+
+// "static void main" must be defined in a public class.
+public class Main {
+    public static void main(String[] args) {
+        // initialize
+        String s1 = "Hello World";
+        System.out.println("s1 is \"" + s1 + "\"");
+        String s2 = s1;
+        System.out.println("s2 is another reference to s1.");
+        String s3 = new String(s1);
+        System.out.println("s3 is a copy of s1.");
+
+
+        // compare using '=='
+        // == 比较的的是引用的地址
+        System.out.println("Compared by '==':");
+        // true since string is immutable and s1 is binded to "Hello World"
+        System.out.println("s1 and \"Hello World\": " + (s1 == "Hello World"));
+        // true since s1 and s2 is the reference of the same object
+        System.out.println("s1 and s2: " + (s1 == s2));
+        // false since s3 is refered to another new object
+        System.out.println("s1 and s3: " + (s1 == s3));
+
+
+        // compare using 'equals'
+        // String类的equals方法被改写过，只要字符串的内容相等即返回true
+        System.out.println("Compared by 'equals':");
+        System.out.println("s1 and \"Hello World\": " + s1.equals("Hello World"));
+        System.out.println("s1 and s2: " + s1.equals(s2));
+        System.out.println("s1 and s3: " + s1.equals(s3));
+
+
+        // compare using 'compareTo'
+        // String类的compareTo方法实现是直接比较字符串的值
+        System.out.println("Compared by 'compareTo':");
+        System.out.println("s1 and \"Hello World\": " + (s1.compareTo("Hello World") == 0));
+        System.out.println("s1 and s2: " + (s1.compareTo(s2) == 0));
+        System.out.println("s1 and s3: " + (s1.compareTo(s3) == 0));
+    }
+}
